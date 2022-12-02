bba9160c743e098bf46802ab664e4a5c8503b94b
Jesse Evans <jsje@google.com>
2022-12-01 22:16:05 -0800

    Initial checkin + Problem 1

---
+++ src/main/kotlin/ElfCalorieParser.kt
@@ -0,0 +1,21 @@
+import java.io.File
+
+class ElfCalorieParser(val inputFile: File) {
+  fun findCaloriesForElfWithMostCalories(): Int {
+    var maxValue = Int.MIN_VALUE
+    inputFile.useLines {
+      it.fold(0) { sum, line ->
+        when (line.trim()) {
+          "" -> {
+            maxValue = maxOf(sum, maxValue)
+            0
+          }
+          else -> {
+            sum + line.toInt()
+          }
+        }
+      }
+    }
+    return maxValue
+  }
+}
---
+++ src/test/kotlin/ElfCalorieParserTest.kt
@@ -0,0 +1,14 @@
+import java.io.File
+import java.util.ResourceBundle
+import org.junit.jupiter.api.Assertions.*
+
+import org.junit.jupiter.api.Test
+
+internal class ElfCalorieParserTest {
+
+  @Test
+  fun findCaloriesForElfWithMostCalories() {
+    val parser = ElfCalorieParser(File(javaClass.getResource("/input.txt").file))
+    println(parser.findCaloriesForElfWithMostCalories())
+  }
+}
