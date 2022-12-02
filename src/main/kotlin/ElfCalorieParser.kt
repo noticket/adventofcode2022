540d3f67ad5710ee73e59e2e3b37c1c02127d232
Jesse Evans <jsje@google.com>
2022-12-01 22:19:47 -0800

    Fix newline addition

--- src/main/kotlin/ElfCalorieParser.kt
+++ src/main/kotlin/ElfCalorieParser.kt
@@ -3,8 +3,8 @@ import java.io.File
 class ElfCalorieParser(val inputFile: File) {
   fun findCaloriesForElfWithMostCalories(): Int {
     var maxValue = Int.MIN_VALUE
-    inputFile.useLines.plus("") {
-      it.fold(0) { sum, line ->
+    inputFile.useLines {
+      it.plus("").fold(0) { sum, line ->
         when (line.trim()) {
           "" -> {
             maxValue = maxOf(sum, maxValue)
